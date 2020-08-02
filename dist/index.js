9f2502fd62256de0d51e3f9810d3f2995a91e30e
Steve Thompson <writetome51@gmail.com>
2020-08-02 00:48:49 -0600

    safety commit

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var error_if_not_finite_number_1 = require("error-if-not-finite-number");
-function inRange(range, num) {
+import { errorIfNotFiniteNumber } from 'error-if-not-finite-number';
+export function inRange(range, num) {
     if (range.length !== 2) {
-        throw new Error("The range array must have exactly 2 numbers");
+        throw new Error(`The range array must have exactly 2 numbers`);
     }
     errorIfAnyArgumentIsNotNumber();
     if (range[0] > range[1]) {
@@ -12,10 +10,9 @@ function inRange(range, num) {
     }
     return (num >= range[0] && num <= range[1]);
     function errorIfAnyArgumentIsNotNumber() {
-        var args = range.concat([num]);
-        var i = -1;
+        let args = range.concat([num]);
+        let i = -1;
         while (++i < args.length)
-            error_if_not_finite_number_1.errorIfNotFiniteNumber(args[i]);
+            errorIfNotFiniteNumber(args[i]);
     }
 }
-exports.inRange = inRange;
--- dist/tests.js
+++ dist/tests.js
@@ -1,65 +1,63 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("./index");
+import { inRange } from './index';
 // Test 1
-if (index_1.inRange([1, 10], 0))
+if (inRange([1, 10], 0))
     console.log('test 1 failed');
 else
     console.log('test 1 passed');
 // Test 2
-if (index_1.inRange([1, 10], 11))
+if (inRange([1, 10], 11))
     console.log('test 2 failed');
 else
     console.log('test 2 passed');
 // Test 3
-if (index_1.inRange([1, 10], 1))
+if (inRange([1, 10], 1))
     console.log('test 3 passed');
 else
     console.log('test 3 failed');
 // Test 4
-if (index_1.inRange([1, 10], 10))
+if (inRange([1, 10], 10))
     console.log('test 4 passed');
 else
     console.log('test 4 failed');
 // Test 5
-if (index_1.inRange([-10, 0], -11))
+if (inRange([-10, 0], -11))
     console.log('test 5 failed');
 else
     console.log('test 5 passed');
 // Test 6
-if (index_1.inRange([-10, 0], -10))
+if (inRange([-10, 0], -10))
     console.log('test 6 passed');
 else
     console.log('test 6 failed');
 // Test 7
-if (index_1.inRange([-10, 0], 1))
+if (inRange([-10, 0], 1))
     console.log('test 7 failed');
 else
     console.log('test 7 passed');
 // Test 8
-if (index_1.inRange([-10, 0], 0))
+if (inRange([-10, 0], 0))
     console.log('test 8 passed');
 else
     console.log('test 8 failed');
 // Test 9
-if (index_1.inRange([10, 50], 10))
+if (inRange([10, 50], 10))
     console.log('test 9 passed');
 else
     console.log('test 9 failed');
 // Test 10
-if (index_1.inRange([10, 50], 50))
+if (inRange([10, 50], 50))
     console.log('test 10 passed');
 else
     console.log('test 10 failed');
 // Test 11
-if (index_1.inRange([9.5, 20.2], 9.49))
+if (inRange([9.5, 20.2], 9.49))
     console.log('test 11 failed');
 else
     console.log('test 11 passed');
 // Test 12
-var errorTriggered = false;
+let errorTriggered = false;
 try {
-    index_1.inRange([20, 10], 6);
+    inRange([20, 10], 6);
 }
 catch (e) {
     errorTriggered = true;
@@ -71,7 +69,7 @@ else
 // Test 13
 errorTriggered = false;
 try {
-    index_1.inRange(6, [0, 10]);
+    inRange(6, [0, 10]);
 }
 catch (e) {
     errorTriggered = true;
@@ -83,7 +81,7 @@ else
 // Test 14
 errorTriggered = false;
 try {
-    index_1.inRange([0, 10]);
+    inRange([0, 10]);
 }
 catch (e) {
     errorTriggered = true;
@@ -95,7 +93,7 @@ else
 // Test 15
 errorTriggered = false;
 try {
-    var x = index_1.inRange([1], 5);
+    var x = inRange([1], 5);
 }
 catch (e) {
     errorTriggered = true;
@@ -107,7 +105,7 @@ else
 // Test 16
 errorTriggered = false;
 try {
-    var x = index_1.inRange();
+    var x = inRange();
 }
 catch (e) {
     errorTriggered = true;
